184ef4f764e4c8590dbe3e8db9918a713c554781
enimatek-nl <accounts@enimatek.nl>
2021-08-23 21:24:52 +0200

    commit

---
+++ ES1LD.ino
@@ -0,0 +1,308 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+
+// Segway Ninebot ES1LD
+// Author: github.com/enimatek-nl
+
+// Header: BYTE 1 & 2
+#define NINEBOT_H1 0x5A
+#define NINEBOT_H2 0xA5
+// Source: BYTE 3 & Destination: BYTE 4 (NINEBOT)
+#define WIRED_SERIAL 0x3D
+#define BLUETOOTH_SERIAL 0x3E
+#define UNKNOWN_SERIAL 0x3F
+// Command: BYTE 4 (NINEBOT)
+#define SPEED 0x64 // ONLY AT DESTINATION 0x21 ESC>BLE
+
+const String VERSION = "2.3";
+bool DEBUG_MODE = false;                          // Enable debug mode - print details through serial-output
+const int SERIAL_PIN = 2;                         // Pin of serial RX line from ESC
+const int THROTTLE_PIN = 10;                      // PWM pin of throttle
+const int LED0_PIN = 13;                          // Communicate some info through a led
+const int SPEED_MIN = 5;                          // Minium speed to activate/deactivate throttle
+const int SPEED_MAX = 17;                         // Max speed before max throttle is needed (max-specs - 3km/h?)
+const int KICKS_RESET_TRESHOLD = 1;               // After X kicks next one will reset speed-building lock
+const int THROTTLE_STOP = 45;                     // Stop value for throttle (45 anti KERS).
+const int THROTTLE_MIN = 90;                      // Min value for first boost (±7km/h 110?)
+const int THROTTLE_STEP = 10;                     // Each step until SPEED_MAX
+const int THROTTLE_MAX = 233;                     // Max known value for throttle (233 most cases)
+const int THROTTLE_TUNE = 5;                      // tuning value for dynamic dec-/inc-rease of throttle based on targetSpeed
+const unsigned long DELAY_KICK_DETECTION = 350;   // delay between kick detections
+const unsigned long DELAY_KEEP_THROTTLE = 25000;  // time to keep the throttle open after kick
+const unsigned long DELAY_SPEED_DETECTION = 2000; // prevent target speed changes during kicks
+const int SPEED_HIST_SIZE = 20;                   // amount of data points in history
+
+//
+// Start of actual code, don't change stuff below this line when in doubt...
+//
+
+uint8_t serialHeader1 = 0x00;
+uint8_t serialHeader2 = 0x00;
+uint8_t buff[256];   // Reserved for serial packet reading
+int pSizeOffset = 4; // Packet offset
+int pSpeed = 0;      // Packet speed
+
+bool pauseKickDetection = false;
+int intermediateKicks = 0;
+
+unsigned long currentTime = 0;
+unsigned long kickResetTimer = 0;
+unsigned long throttleStopTimer = 0;
+unsigned long speedTargetUnlockTimer = 0;
+
+int averageSpeed = 0;
+int targetSpeed = 0;
+
+int speedHistTotal = 0;         // Sum of the history.
+int speedHist[SPEED_HIST_SIZE]; // History of speeds.
+int speedHistIndex = 0;         // Current index in the history.
+
+SoftwareSerial SoftSerial(SERIAL_PIN, 3); // RX, TX
+
+void setup()
+{
+    if (DEBUG_MODE)
+        Serial.begin(115200);
+
+    SoftSerial.begin(9600); // 9600 baud voor dit model
+
+    if (DEBUG_MODE)
+        Serial.println("ES1LD Firmware " + VERSION + " Ready.");
+
+    TCCR1B = TCCR1B & 0b11111001; // PIN 9 & 10 to 32 khz PWM
+
+    pinMode(LED0_PIN, OUTPUT);
+    digitalWrite(LED0_PIN, LOW);
+
+    analogWrite(THROTTLE_PIN, THROTTLE_STOP);
+}
+
+uint8_t readByte()
+{
+    while (!SoftSerial.available())
+        delay(1);
+    return SoftSerial.read();
+}
+
+void loop()
+{
+    if (serialHeader1 == 0x00)
+    {
+        switch (readByte())
+        {
+        case NINEBOT_H1:
+            if (readByte() == NINEBOT_H2)
+            {
+                if (DEBUG_MODE)
+                    Serial.println("!! Detected NineBot Protocol");
+                serialHeader1 = NINEBOT_H1;
+                serialHeader2 = NINEBOT_H2;
+            }
+            else
+            {
+                if (DEBUG_MODE)
+                    Serial.println("?? Unknown NineBot Protocol");
+            }
+            break;
+        default:
+            if (DEBUG_MODE)
+                Serial.println("!! Detected Unknown Protocol");
+            break;
+        }
+    }
+    else
+    {
+        while (readByte() != serialHeader1)
+            ; // Waiting for header1
+        if (readByte() != serialHeader2)
+            return;                 // Second byte does not match
+        uint8_t pSize = readByte(); // Byte 3 = packet size
+        if (pSize < 3 || pSize > 8)
+        {
+            if (DEBUG_MODE)
+                Serial.println((String) "!! Incorrect pSize of " + pSize);
+            return; // Incorrect packet size
+        }
+        buff[0] = pSize;
+        uint8_t r = 0x00;
+        uint16_t sum = pSize;
+        for (int i = 0; i < pSize + pSizeOffset; i++)
+        {
+            r = readByte();
+            buff[i + 1] = r;
+            sum += r;
+        }
+        uint16_t checksum = (uint16_t)readByte() | ((uint16_t)readByte() << 8); // Last 2 bytes contain checksum
+        if (checksum != (sum ^ 0xFFFF))
+        {
+            if (DEBUG_MODE)
+                Serial.println((String) "!! Invalid checksum: " + checksum + " != " + (sum ^ 0xFFFF));
+            return;
+        }
+
+        else if (buff[2] == 0x21 && buff[3] == SPEED)
+        {
+            pSpeed = buff[pSize + pSizeOffset - 1];
+            speedHistTotal = speedHistTotal - speedHist[speedHistIndex];
+            speedHist[speedHistIndex] = pSpeed;
+            speedHistTotal = speedHistTotal + speedHist[speedHistIndex];
+            speedHistIndex = speedHistIndex + 1;
+            if (speedHistIndex >= SPEED_HIST_SIZE)
+            {
+                speedHistIndex = 0;
+            }
+            averageSpeed = speedHistTotal / SPEED_HIST_SIZE;
+
+            if (pSpeed != 0 && pSpeed < SPEED_MIN)
+            {
+                targetSpeed = 0; // Going to slow or brake, disable throttle
+            }
+            else
+            {
+                kickDetection();
+            }
+
+            if (DEBUG_MODE && pSpeed != 0)
+                Serial.println((String) "?? pSpeed changed: " + pSpeed + ", Avg: " + averageSpeed + ", kickPaused:" + pauseKickDetection);
+        }
+
+        else
+        {
+            if (buff[3] != SPEED)
+            {
+                if (DEBUG_MODE)
+                {
+                    Serial.println("!! Unknown packet:");
+                    for (int i = 0; i < pSize + pSizeOffset; i++)
+                    {
+                        Serial.print(buff[i], HEX);
+                        Serial.print(" ");
+                    }
+                    Serial.println("END.");
+                }
+            }
+        }
+
+        analogWrite(THROTTLE_PIN, calcThrottle());
+
+        // Timers... without heavy lib
+        currentTime = millis();
+        if (kickResetTimer != 0 && kickResetTimer + DELAY_KICK_DETECTION < currentTime)
+            resetKickDetection();
+        if (throttleStopTimer != 0 && throttleStopTimer + DELAY_KEEP_THROTTLE < currentTime)
+            stopThrottle();
+        if (speedTargetUnlockTimer != 0 && speedTargetUnlockTimer + DELAY_SPEED_DETECTION < currentTime)
+            unlockSpeedTarget();
+    }
+}
+
+int getHighestSpeed()
+{
+    int s = 0;
+    for (int i = 0; i < SPEED_HIST_SIZE; i++)
+    {
+        if (speedHist[i] > s)
+            s = speedHist[i];
+    }
+    return s;
+}
+
+void kickDetection()
+{
+    int tier = calcTier(); // Calculate tier based on current speed, starting speed is higher to prevent differences.
+
+    if (averageSpeed > SPEED_MIN && (pSpeed - averageSpeed) >= tier && !pauseKickDetection)
+    {
+        if (speedTargetUnlockTimer == 0 || intermediateKicks >= KICKS_RESET_TRESHOLD)
+        {
+            targetSpeed = pSpeed >= SPEED_MAX ? SPEED_MAX : pSpeed;
+
+            if (DEBUG_MODE)
+                Serial.println((String) "!! Kick Detected, current step speed: " + pSpeed + ", target speed: " + targetSpeed);
+
+            digitalWrite(LED0_PIN, HIGH);
+            intermediateKicks = 0;
+            pauseKickDetection = true;
+            kickResetTimer = currentTime;
+            enqueueStopThrottle();
+        }
+        else
+        {
+            intermediateKicks++; // increase kick counter and based on that we slowly increase the target speed?
+        }
+    }
+    else if (averageSpeed > SPEED_MIN && pSpeed < averageSpeed)
+    {
+        // TODO: some magic detection of brake and/or hill?
+    }
+}
+
+// Dynamically allocate throttle based on the speed instead of the throttle itself.
+int calcThrottle()
+{
+    if (targetSpeed > SPEED_MIN)
+    {
+        if (targetSpeed > SPEED_MAX)
+        {
+            return THROTTLE_MAX;
+        }
+        else
+        {
+            int d = (targetSpeed - averageSpeed);
+            d = d * THROTTLE_TUNE; // small increase (or decrease) the throttle based on current speed compared to the average
+            int t = (THROTTLE_MIN + d) + (THROTTLE_STEP * (targetSpeed - SPEED_MIN));
+            return t > THROTTLE_MAX ? THROTTLE_MAX : t;
+        }
+    }
+    else
+    {
+        return THROTTLE_STOP;
+    }
+}
+
+int calcTier()
+{
+    if (averageSpeed > 14)
+        return 1;
+    if (averageSpeed >= 5)
+        return 2;
+    return 3;
+}
+
+void unlockSpeedTarget()
+{
+    if (intermediateKicks > 0)
+    {
+        enqueueStopThrottle();
+    }
+    else
+    {
+        speedTargetUnlockTimer = 0;
+        digitalWrite(LED0_PIN, LOW);
+    }
+    intermediateKicks = 0;
+    if (DEBUG_MODE)
+        Serial.println("!! Speed Target unlocked, resetting kickcounter.");
+}
+
+void resetKickDetection()
+{
+    kickResetTimer = 0;
+    pauseKickDetection = false;
+    if (DEBUG_MODE)
+        Serial.println("!! Kick Detection resumed.");
+}
+
+void enqueueStopThrottle()
+{
+    speedTargetUnlockTimer = currentTime;
+    throttleStopTimer = currentTime;
+}
+
+void stopThrottle()
+{
+    targetSpeed = 0;
+    throttleStopTimer = 0;
+    if (DEBUG_MODE)
+        Serial.println("!! Throttle stopped, time for a kick.");
+}
--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # ninebot-legaliseren
-Legaliseer Ninebot by Segway ES1LD voor 't gebruik als e-bike op de openbare weg
+Legaliseer Ninebot by Segway ES1LD voor 't gebruik als e-bike op de openbare weg.
+
+# Hoe sluit ik de Arduino Nano aan?
+![Wiring Scheme](schema.png?raw=true "Wiring Scheme")
+
+# Hoe zet ik de firmware in de Arduino?
+Installer de [Arduino IDE](https://www.arduino.cc) en selecteer oa. de Nano onder 'Tools' als 'Board' en zet de juiste poort.
+Open de .ino file uit deze repository en selecteer vervolgens 'Upload' onder 'Sketch' menu.
+
+# Uitleg
+Onder de 'SPEED_MIN' km/h doet de ondersteuning niks.
+
+Bij een 'kick' (= afzetje) en het verschil tussen stilstand en rijden is >= 3km/h dan start de ondersteuning.
+
+Na 'DELAY_KEEP_THROTTLE' ms, indien er geen enkele kick is gegeven bij welke ondersteuning ook, zal de snelheid weer worden afgebouwd.
+
+Of als de snelheid onder de 'SPEED_MIN' km/h komt, stop de ondersteuning; bijv bij remmen, maar dit kan ook bij stijle heuvels.
+
+### Stap 1
+De (eerste) 'kick' snelheid zal vast worden gezet (waarschijnlijk ±7km/h) en voor 'DELAY_SPEED_DETECTION' ms worden opgebouwd.
+In deze tijd kan een 2e kick worden gegeven om de snelheid te verlengen.
+
+### Stap 2
+
+Wacht je na Stap 1 > 'DELAY_SPEED_DETECTION' ms of geef je in de tussentijd 'KICKS_RESET_TRESHOLD' extra kick's dan zal zal de Stap 1 cyclus worden herhaalt met de hogere snelheid van dat moment.
+
+Simpel. 'trage' hardere kicks versnelt & veel korte kicks achter elkaar.
+
+Remmen? gebruik gewoon de aanwezige remmen.
+
+
+# Ministerie van Infrastructuur en Waterstaat
+> Voor e-steps met trapondersteuning gelden dezelfde regels als voor een e-bike: u moet zelf meetrappen om vooruit te komen, de trapondersteuning stopt bij 25 kilometer per uur en het maximale motorvermogen is 250 watt. De step mag dus niet zelfrijdend zijn. Zie ook https://www.rijksoverheid.nl/onderwerpen/fiets/vraag-en-antwoord/welke-regels-gelden-voor-mijn-elektrische-fiets-e-bike.  
+> 
+> E-steps met trapondersteuning moeten ook voldoen aan de 'permanente eisen' voor fietsen zoals die staan in de Regeling voertuigen (hoofdstuk 5, afdeling 9): https://wetten.overheid.nl/jci1.3:c:BWBR0025798&hoofdstuk=5&afdeling=9&z=2021-01-21&g=2021-01-21.  
+> 
+> We hopen u hiermee voldoende te hebben geïnformeerd.
+> 
+> Met vriendelijke groet,
+> 
+> Ministerie van Infrastructuur en Waterstaat
+> DGMo | Burgerbrieven 
